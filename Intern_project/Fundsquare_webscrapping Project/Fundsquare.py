0ed1b2296fe0685aa18dcff8c555966c43d7f25f
Ukpro-code <ukproworkspace@gmail.com>
2025-06-29 18:59:59 +0530

    Latest File

--- Intern_project/Fundsquare_webscrapping Project/Fundsquare.py
+++ Intern_project/Fundsquare_webscrapping Project/Fundsquare.py
@@ -1,6 +1,4 @@
 import time
-start_time = time.time()  # <-- Move this here, right after imports
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,6 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import os
+
+start_time = time.time()  # <-- Move this here, right after imports
 
 username = "ukprowork"
 password = "5b-G5s.*s4hPqEn"
