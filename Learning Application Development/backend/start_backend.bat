168b78cbac2b4f5c93e0f78b255908b00120b507
Ukpro-code <ukproworkspace@gmail.com>
2026-02-10 19:06:30 +0530

    latest update

--- Learning Application Development/backend/start_backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "D:\Ukesh\Coding Shinobi\Coding-Shinobi\Learning Application Development\backend"
-set PYTHONPATH=D:\Ukesh\Coding Shinobi\Coding-Shinobi\Learning Application Development\backend
-"D:\Ukesh\Coding Shinobi\.conda\python.exe" -m uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
-pause
